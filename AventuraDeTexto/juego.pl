4165f10d8f003aadc6d746bf5aeaad15b31dc3d2
UrrIsaac <isaacurri@ciencias.unam.mx>
2026-05-18 19:08:09 -0600

    feat[juego.pl] : añade guía 1 de ideas.

---
+++ AventuraDeTexto/juego.pl
@@ -0,0 +1,18 @@
+% Guía para realización del juego
+% Dentro del archivo decisiones.txt estará la estructura del juego, contemplando la gran mayoria de texto a poner en el juego
+
+% El motivo de la guía es dar especificaciones generales y un panorama para
+% poder tener establecidos desde antes de comenzar las caracteristicas del juego.
+
+% Quizás no sean muchas opciones dentro del árbol para completar le juego, pero habrán capas para avanzar.
+
+% Contaremos con 3 stats (propuesta) : descanso, estrés y conocimiento, las tres con la escala de 0 = bajo, 3 = alto. 
+%Tenerlo alto es bueno en "descanso" y "conocimiento", es malo tener el estrés alto
+
+% La idea es que dependiendo de nuestros stats podamos acceder a una u otra respuesta, para así poder avanzar tendremos que ir pasando por las opciones.
+% Puede que una mala decisión te haga tener demasiado estrés y poco conocimiento, lo cual resulte en llegar a una mala resolución.
+
+% Como el juego no debe tener una dificultad muy pequeña, la idea fue esa (añadir stats que nos condiciones) 
+% para pelearnos con la forma en la que midamos las cosas y lleguemos a diversos escenarios
+
+
