9334e3966b179d8a26bc9b7f0de77a130a6b01a2
PippuPippu17 <PippuPippu17@users.noreply.github.com>
2026-05-23 18:17:57 -0600

    COmandos y logica

--- BusquedaTesoro/BusquedaDelTesoro.pl
+++ BusquedaTesoro/BusquedaDelTesoro.pl
@@ -62,3 +62,130 @@
         "La vida está llena de altibajos, pero a la larga te acostumbras",
         "01011000 01101001 01101101 01100101 01101110 01100001 00100000 01110110 01110101 01100101 01101100 01110110 01100101 00100000 01110000 01101111 01110010 00100000 01100110 01100001 01110110 01101111 01110010 00100000 01110100 01100101 00100000 01100101 01111000 01110100 01110010 01100001 11000011 10110001 01101111"
     ]).
+
+/*
+ Comandos para iniciar el juego, mostrar los lugares, usar objetos, y salir del juego
+*/
+    % Permite al jugador usar un objeto de su inventario, si lo tiene, y muestra un mensaje indicando que se ha usado el objeto. Si el jugador no tiene el objeto, muestra un mensaje de error.
+    usar(Objeto) :-
+        inventario(Objeto), % Verifica si el objeto esta en el inventario del jugador
+        nl,
+        write("Usaste: "), write(Objeto), nl.
+
+    % Mensaje de error si el jugador intenta usar un objeto que no tiene en su inventario
+    usar(_) :-
+        nl,
+        write("No tienes ese objeto."), nl.
+
+    % Permite al jugador ver la lista de lugares disponibles en el mapa, mostrando un mensaje de encabezado y luego iterando sobre la lista de lugares para imprimir cada uno de ellos. Si el jugador no ingresa un comando válido, muestra un mensaje de error.
+    lugares :- 
+        mostrar_lugares.
+
+    % Mensaje de error si el jugador ingresa un comando que no coincide con ninguno de los comandos disponibles
+    salir :-
+        nl,
+        write("Gracias por jugar."), nl,
+        halt.
+
+    % Mensaje de error si el jugador ingresa un comando que no coincide con ninguno de los comandos disponibles
+    mostrar_lugares :-
+        nl,
+        write("Ubicaciones Disponibles:"), nl,
+        % Imprime la lista de lugares
+        forall(
+            lugar(Nombre, _, _), % Itera sobre cada lugar definido en el juego
+            (write("- "), write(Nombre), nl) % Imprime el nombre de cada lugar
+        ), nl.
+
+    ayuda :-
+        nl,
+        write("COMANDOS: "), nl,
+        write("lugares.           -> Ver lugares"), nl,
+        write("ir(Lugar).         -> Viajar"), nl,
+        write("estado.            -> Ver estado"), nl,
+        write("inventario_j.      -> Ver inventario"), nl,
+        write("hablar(Persona).   -> Interactuar"), nl,
+        write("buscar.            -> Buscar tesoro"), nl,
+        write("usar(Objeto).      -> Usar objeto"), nl,
+        write("ayuda.             -> Mostrar ayuda"), nl,
+        write("salir.             -> Salir del juego"), nl,
+        nl.
+
+    % Borra todo para volver a iniciar
+    limpiar_estado :-
+        retractall(jugador_pos(_)), % Elimina la posición del jugador
+        retractall(inventario(_)), % Elimina los objetos del inventario
+        retractall(objeto_en(_, _)), % Elimina la ubicación de los objetos
+        retractall(tesoro_en(_)), % Elimina la ubicación del tesoro
+        retractall(pista_descartada(_)), % Elimina las pistas descartadas
+        retractall(chocolate_protegido), % Elimina el estado de protección del chocolate
+        retractall(inteligencia_maxima), % Elimina el estado de inteligencia maxima
+        retractall(odin_activo). % Elimina el estado de Odin activo
+
+
+    % Inicializa, restaura los estados del juego y presenta la introducción al jugador
+    iniciar :-
+        limpiar_estado, 
+        inicializar_objetos, % distribuye objetos aleatoriamente por el mapa
+        inicializar_tesoro, % esconde el tesoro aleatoriamente
+        assertz(jugador_pos(media_luna)), % Pone al jugador en el lugar incial
+        nl, 
+        % Mensaje de incio del juego
+        write("=========================================="), nl,
+        write(" Búsqueda del tesoro"), nl,
+        write("=========================================="), nl,
+        nl,
+        write("Después de muchos obstaculos descubriste el mapa de Midivlar... "), nl,
+        write("Tras desifrarlo te dirigiste a la Facultad de Ciencias para "), nl,
+        write("tener la sabiduría de las Mentitas sagradas."), nl,
+        ayuda, % Muestra los comandos disponibles al iniciar el juego
+        estado. % Muestra el estado actual del jugador
+
+/*
+ Lógica para moverse entre los diferentes lugares del mapa, 
+*/
+    ir(Lugar) :-
+        lugar(Lugar, _, _), % Verifica que el lugar exista
+        retract(jugador_pos(_)), % Elimina la posición actual del jugador
+        assertz(jugador_pos(Lugar)), % Actualiza la posición del jugador al nuevo lugar
+        estado,
+        verificar_tesoro.
+    % Imprime mensaje de error si el lugar no coincide con ninguno de los lugares disponibles
+    ir(_) :-
+        nl,
+        write("Ese lugar no existe."), nl.
+
+/*
+  Lógica para esconder el tesoro en una de las 20 ubicaciones
+*/
+    inicializar_tesoro :-
+        findall(L, lugar(L, _, _), Lugares),
+        random_member(Lugar, Lugares),
+        assertz(tesoro_en(Lugar)).
+
+/*
+ Lógica para buscar el tesoro en la ubicación actual 
+*/
+buscar :-
+    jugador_pos(Pos), % Obtiene la posición actual del jugador
+    tesoro_en(Pos), % Verifica si el tesoro esta en la ubicación actual del jugador
+    nl,
+    % Imprime el mensaje de que el tesoro fue encontrado
+    write("HAS ENCONTRADO EL COFRE!"), nl,
+    write("El fantasma de Midivlar aparece..."), nl,
+    batalla_final. % Inicia la batalla final
+
+buscar :-
+    % Imprime el mensaje de que el tesoro no esta en la ubicación actual del jugador
+    nl,
+    write("No encuentras nada sospechoso aqui."), nl.
+
+verificar_tesoro :-
+    jugador_pos(Pos), % Obtiene la posición actual del jugador
+    tesoro_en(Pos), % Verifica si el tesoro esta en la ubicación actual del jugador
+    nl,
+    % Imprime el mensaje que indica que el jugador esta en la ubicación del tesoro.
+    write("Sientes una presencia extrana..."), nl,
+    !.
+
+verificar_tesoro.
